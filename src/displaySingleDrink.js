1f3661429780eca1748509bd71ed7804e0c6f755
Dwhitewolf1995 <shamil.musaev@iths.se>
2023-06-28 17:30:01 +0200

    Can display a drink now

--- src/displaySingleDrink.js
+++ src/displaySingleDrink.js
@@ -1,9 +1,38 @@
 import { hideLoading } from "./toggleLoading.js";
-import get from './getElement.js'
+import get from "./getElement.js";
 
-const displayDrink = (drink) => {
-    hideLoading()
-    console.log(drink);
-}
+const displayDrink = (data) => {
+  hideLoading();
 
-export default displayDrink
+  const drink = data.drinks[0];
+  const { strDrinkThumb: image, strDrink: name, strInstructions: desc } = drink;
+  const list = [
+    drink.strIngredient1,
+    drink.strIngredient2,
+    drink.strIngredient3,
+    drink.strIngredient4,
+    drink.strIngredient5
+];
+//Получаем элементы
+const img = get('.drink-img')
+const drinkName = get('.drink-name')
+const description = get('.drink-desc')
+const ingredients = get('.drink-ingredients')
+
+//Передаем картинку 
+
+img.src = image
+drinkName.textContent = name
+description.textContent = desc
+ingredients.innerHTML = list.map((item) => {
+if(!item) return
+return `<li><i class="far fa-check-square"></i>${item}</li>`
+}).join('')
+
+//Меняем title из singleDrink.html динамически
+document.title = name
+
+  console.log(drink,list);
+};
+
+export default displayDrink;
